7596b8dd9f7ee721ed21dbe074d0a73f4b8d3296
Pavel Burgr <pb@drake.cz>
2023-01-03 18:13:15 +0100

    WIP

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(
-    version="1.0.0",
-    py_modules=["virtual_timer"],
-)
--- virtual_timer.py
+++ virtual_timer.py
@@ -1,9 +1,52 @@
 from machine import Timer
 
-next_id = -1
+default_scheduler = None
 
-def new():
-    global next_id
-    id = next_id
-    next_id = next_id - 1
-    return Timer(id)
+class VirtualTimerScheduler:
+    counter = 0
+    period = 0
+    timers = list()
+
+    def __init__(self, hw_timer, period):
+
+        def callback(timer):
+            for timer in self.timers:
+                if self.counter % timer.period == 0:
+                    timer.callback(timer)
+
+            self.counter += 1
+
+        self.period = period
+        hw_timer.init(mode=Timer.PERIODIC, period=period, callback=callback)
+
+    def add(self, timer):
+        self.timers.append(timer)
+
+    def remove(self, timer):
+        self.timers.remove(timer)
+
+class VirtualTimer:
+
+    period = 0
+    scheduler = None
+    callback = None
+
+    def init(self, mode=Timer.PERIODIC, period=-1, callback=None, scheduler=None):
+
+        if scheduler == None:
+            global default_scheduler
+            if default_scheduler == None:
+                default_scheduler = VirtualTimerScheduler(Timer(0), 100)
+
+            scheduler = default_scheduler
+
+        self.scheduler = scheduler
+
+        self.period = period / scheduler.period
+        self.callback = callback
+
+        self.scheduler.add(self)
+
+
+    def deinit(self):
+        self.scheduler.remove(self)
